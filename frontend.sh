364cdeca6a92d565f53e56de36521e4c4218dc32
rajve <rajvel@gmail.com>
2024-05-03 17:45:16 -0400

    troubleshooting frontend steps moving cd up after install

--- frontend.sh
+++ frontend.sh
@@ -1,10 +1,6 @@
 dnf install nginx -y
 cp expense.conf /etc/nginx/default.d/expense.conf
 
-
-systemctl enable nginx
-systemctl start nginx
-
 rm -rf /usr/share/nginx/html/*
 
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
@@ -12,7 +8,5 @@ cd /usr/share/nginx/html
 
 unzip /tmp/frontend.zip
 
-ls
-pwd
-
 systemctl restart nginx
+systemctl enable nginx
