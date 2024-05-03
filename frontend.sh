a479edc658bd37e2e2966c8f170bd9e514292669
rajve <rajvel@gmail.com>
2024-05-03 17:19:22 -0400

    Create frontend script

--- frontend.sh
+++ frontend.sh
@@ -6,9 +6,9 @@ systemctl start nginx
 rm -rf /usr/share/nginx/html/*
 
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
+cd /usr/share/nginx/html
 
 unzip /tmp/frontend.zip
 
-cd /usr/share/nginx/html
 # Copy file step pending
 systemctl restart nginx
