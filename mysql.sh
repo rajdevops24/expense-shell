f22ef67154cde39af7bb31d98726b2fdf6f2dc02
rajve <rajvel@gmail.com>
2024-05-03 22:54:53 -0400

    variable substitution for mysql server

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,12 @@
-dnf install mysql-server -y
+echo -e "\e[31mInstalling Mysql Server..\e[0m"
+dnf install mysql-server -y &> /tmp/expensemysql.log
+echo exit status - $?
 
-systemctl enable mysqld
-systemctl start mysqld
+echo -e "\e[31m Starting Services..\e[0m"
+systemctl enable mysqld &>> /tmp/expensemysql.log
+systemctl start mysqld &>> /tmp/expensemysql.log
+echo exit status - $?
 
-mysql_secure_installation --set-root-pass $1
+echo -e "\e[31m secure installation..\e[0m"
+mysql_secure_installation --set-root-pass $1 &>> /tmp/expensemysql.log
+echo exit status - $?
