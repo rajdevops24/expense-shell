00d30faee81a9a0a6dd3dc3d44a310f8001567b3
rajve <rajvel@gmail.com>
2024-05-03 22:17:11 -0400

    redirectors with append

--- frontend.sh
+++ frontend.sh
@@ -11,16 +11,16 @@ rm -rf /usr/share/nginx/html/* &> /tmp/expense.log
 echo exit status - $?
 
 echo -e "\e[31mDownloading front end content..\e[0m"
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip &> /tmp/expense.log
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip &>> /tmp/expense.log
 echo exit status - $?
 
 cd /usr/share/nginx/html
 
 echo -e "\e[31mExtracting content..\e[0m"
-unzip /tmp/frontend.zip &> /tmp/expense.log
+unzip /tmp/frontend.zip &>> /tmp/expense.log
 echo exit status - $?
 
 echo -e "\e[31mRestarting the service..\e[0m"
-systemctl restart nginx
-systemctl enable nginx
+systemctl restart nginx &>> /tmp/expense.log
+systemctl enable nginx &>> /tmp/expense.log
 echo exit status - $?
